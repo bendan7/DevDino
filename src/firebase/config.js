0cef3c062a5336a324e0cc14e078eeda28f7cdbb
Ben Dan <bend@amdocs.com>
2022-02-04 10:14:46 +0200

    Connect project to firebase...

---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import  { initializeApp } from "firebase/app";
+import 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDRT9OUHgMwICFj6h3Dzm9jrShuPweEwV8",
+  authDomain: "dinodev-75a75.firebaseapp.com",
+  projectId: "dinodev-75a75",
+  storageBucket: "dinodev-75a75.appspot.com",
+  messagingSenderId: "848344230516",
+  appId: "1:848344230516:web:c574c827ed63c9fc71ff21"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
