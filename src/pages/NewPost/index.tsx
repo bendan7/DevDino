734204efdd5dbc47217360e2b95598801b2cf654
Ben Dan <bend@amdocs.com>
2022-02-06 11:55:52 +0200

    Adding new post form

--- src/pages/NewPost/index.tsx
+++ src/pages/NewPost/index.tsx
@@ -1,10 +1,24 @@
+import { Form, Button } from "react-bootstrap";
 import RichEditorExample from "../../components/RichTextEditor";
 
 export default function NewPostPage() {
   return (
     <div style={{ padding: "1rem 0" }}>
       <h2>New Post</h2>
-      <RichEditorExample />
+      <Form>
+        <Form.Group className="mb-3" controlId="AuthorName">
+          <Form.Label>Author name</Form.Label>
+          <Form.Control type="text" placeholder="Enter your name" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="PostTitle">
+          <Form.Label>Post title</Form.Label>
+          <Form.Control type="text" placeholder="Title" />
+        </Form.Group>
+        <RichEditorExample />
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
