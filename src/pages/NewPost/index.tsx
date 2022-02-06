fa772a2b2398db738d1e75e4a029506be8a5a51c
Ben Dan <bend@amdocs.com>
2022-02-06 11:20:51 +0200

    Adding Draft.js RichTextEditor

--- src/pages/NewPost/index.tsx
+++ src/pages/NewPost/index.tsx
@@ -1,7 +1,10 @@
+import RichEditorExample from "../../components/RichTextEditor";
+
 export default function NewPostPage() {
   return (
     <div style={{ padding: "1rem 0" }}>
       <h2>New Post</h2>
+      <RichEditorExample />
     </div>
   );
 }
